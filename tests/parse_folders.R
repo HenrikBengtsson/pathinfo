f3fa0670a6fb4b1ff38cc9ea3ea1caee8a11e6e6
hb <hb@aroma-project.org>
2015-05-18 17:57:01 -0700

    Add more Aroma Framework tests

--- tests/parse_folders.R
+++ tests/parse_folders.R
@@ -1,17 +1,44 @@
 library(pathinfo)
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Aroma Framework
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# annotationData/
 paths <- c(
-  "C:/hb/../annotationData/organism/Homo_sapiens/Ensembl/79/",
-  "../annotationData/organisms/Homo_sapiens/Ensembl/79/",
-  "annotationData/organisms/Homo_sapiens/Ensembl/79/"
+  "annotationData/organisms/Homo_sapiens/Ensembl/79/",
+  "../annotationData,shared/organisms/Homo_sapiens/Ensembl/79/",
+  "C:/hb/../annotationData/organism/Homo_sapiens/Ensembl/79/"
 )
 
 patterns <- c("**", rootPath="annotationData(|,.*)", "organisms",
               organism=".*", "**")
 for (kk in seq_along(paths)) {
   print(paths[kk])
-  mdata <- parse_folders(paths[kk], patterns=patterns, mustWork=FALSE)
+  mraw <- parse_folders(paths[kk], patterns=patterns, mustWork=FALSE)
+  str(mraw)
+  mdata <- as.list(mraw)
   str(mdata)
-  str(as.list(mdata))
+  stopifnot(mdata$organism == "Homo_sapiens")
+}
+
+# rawData/ and probeData/
+paths <- c(
+  "rawData/MyDataSet,UCSF/GenomeWideSNP_6/",
+  "rawData,shared/MyDataSet/GenomeWideSNP_6/",
+  "probeData/MyDataSet,UCSF,ACC/Mapping250K_Nsp/"
+)
+
+patterns <- c("**", rootPath="(raw|probe)Data(|,.*)",
+                    dataset=".*", chiptype=".*")
+for (kk in seq_along(paths)) {
+  print(paths[kk])
+  mraw <- parse_folders(paths[kk], patterns=patterns, mustWork=FALSE)
+  str(mraw)
+  mdata <- as.list(mraw)
+  str(mdata)
+  stopifnot(
+    grepl("MyDataSet", mdata$dataset),
+    mdata$chiptype %in% c("GenomeWideSNP_6", "Mapping250K_Nsp")
+  )
 }
 
